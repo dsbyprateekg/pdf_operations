db50ace6958049526d6de21a5600a25d398dd288
Prateek Gupta <prateek.g@droisys.local>
2020-01-13 12:40:18 +0530

    first commit for pdf basic operations code

---
+++ README.md
@@ -0,0 +1,2 @@
+# Overview
+- This project demonstrates how can we do some basic operations on PDF file using PyPDF2 package
---
+++ extract_basic_pdf_info.py
@@ -0,0 +1,28 @@
+from PyPDF2 import PdfFileReader #pip install pypdf2
+
+
+def extract_information(pdf_path):
+    with open(pdf_path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        information = pdf.getDocumentInfo()
+        number_of_pages = pdf.getNumPages()
+
+    # Python 3’s new f-strings for string formatting
+    txt = f"""
+    Information about {pdf_path}: 
+
+    Author: {information.author}
+    Creator: {information.creator}
+    Producer: {information.producer}
+    Subject: {information.subject}
+    Title: {information.title}
+    Number of pages: {number_of_pages}
+    """
+
+    print(txt)
+    return information
+
+
+if __name__ == '__main__':
+    path = 'The+Machine+Learning+Journey.pdf'
+    extract_information(path)
